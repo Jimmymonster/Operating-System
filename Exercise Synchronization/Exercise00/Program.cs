eaea0d9173a361f1e15e66ac1a3b558d0a114799
Jimmymonster <twosoul1212312121@gmail.com>
2023-09-13 09:35:03 +0700

    synchronization Exe

---
+++ Exercise Synchronization/Exercise00/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Exercise00{
+    class Program{
+        private static int sum=0;
+        static void plus(){
+            int i;
+            for(i=0;i<1000001;i++){
+                sum+=i;
+            }
+        }
+        static void minus(){
+            int i;
+            for(i=0;i<1000001;i++){
+                sum-=i;
+            }
+        }
+        static void Main(string[] args){
+            Stopwatch sw=new Stopwatch();
+            Console.WriteLine("Start...");
+            sw.Start();
+            plus();
+            minus();
+            sw.Stop();
+            Console.WriteLine("sum = {0}",sum);
+            Console.WriteLine("Time used: "+sw.ElapsedMilliseconds.ToString()+"ms");
+        }
+    }
+}
---
+++ Exercise Synchronization/Exercise04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Exercise04{
+    class Program{
+        private static string x="";
+        private static int exitflag=0;
+        static void ThReadX(){
+            while(exitflag=0) Console.WriteLine("X={0}",x);
+        }
+        static void ThWriteX(){
+            string xx;
+            while(exitflag==0){
+                Console.WriteLine("Input: ");
+                xx=Console.ReadLine();
+                if(xx=='exit') exitflag=1;
+                else x=xx;
+            }
+        }
+        static void Main(string[] args){
+            Thread A = new Thread(ThReadX);
+            Thread B = new Thread(ThWriteX);
+
+            A.Start();
+            B.Start();
+        }
+    }
+}
